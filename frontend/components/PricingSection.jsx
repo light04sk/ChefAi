230c59ee8aaab05a579d815454a73e524e66b4ff
light04sk <skmr39630@gmail.com>
2026-04-27 22:07:55 +0530

    fix: remove PricingTable

--- frontend/components/PricingSection.jsx
+++ frontend/components/PricingSection.jsx
@@ -1,21 +1,5 @@
-import { PricingTable } from "@clerk/nextjs";
-
 const PricingSection = () => {
-  return (
-    <div className="w-full [&_.cl-pricingTable]:flex [&_.cl-pricingTable]:flex-col [&_.cl-pricingTable]:gap-4 sm:[&_.cl-pricingTable]:flex-row sm:[&_.cl-pricingTable]:gap-5 [&_.cl-pricingTable]:max-w-none [&_.cl-pricingTable]:w-full [&_.cl-pricingTableCard]:w-full">
-      <PricingTable
-        checkoutProps={{
-          appearance: {
-            elements: {
-              drawerRoot: {
-                zIndex: 2000,
-              },
-            },
-          },
-        }}
-      />
-    </div>
-  );
+  return null;
 };
 
 export default PricingSection;
